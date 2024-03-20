a4bfd6fbb8b338a042d123e52b8dde68b85d2a74
EvangCur <c25curt.evangelister@afacademy.af.edu>
2024-03-19 23:08:28 -0600

    Final code, not tested

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -70,7 +70,15 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 
 --Declare stoplight component here 
-
+component stoplight_fsm is
+    port( i_C     : in  STD_LOGIC;
+           i_reset : in  STD_LOGIC;
+           i_clk   : in  STD_LOGIC;
+           o_R     : out  STD_LOGIC;
+           o_Y     : out  STD_LOGIC;
+           o_G     : out  STD_LOGIC
+     );
+end component;
 
 component clock_divider is
 	generic ( constant k_DIV : natural := 2	);
@@ -91,9 +99,10 @@ begin
 	clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
         generic map ( k_DIV => 50000000 ) -- 1 Hz clock from 100 MHz
         port map (						  
-            i_clk   => 
-            i_reset => 
-            o_clk   => 
+            i_clk   => clk,
+            i_reset => btnL,
+            o_clk   => w_clk
+            
         );    
 	
 end top_basys3_arch;
